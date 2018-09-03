6c96e11e4e0af317473c9999fd01f1038c2e0830
JamesRileydev <jamesriley.dev@gmail.com>
2018-09-03 15:38:25 -0400

    Minor additions

--- jQuery_and_Axios/ajax.js
+++ jQuery_and_Axios/ajax.js
@@ -1,33 +1,52 @@
-window.onload = function() {
+window.onload = function () {
 
-$("#btn").click(function(){
-  // alert("u clicked");
-  $.ajax({
-    method: "GET",
-    url: "https://baconipsum.com/api/?type=meat-and-filler",
-    dataType: 'json'
-  })
-  .done(addP)
-  .fail(function(){
-    alert("FAILURE");
-  })
-});
+  $("#btn").click(function () {
+    // alert("u clicked");
+    $.ajax({
+      method: "GET",
+      url: "https://baconipsum.com/api/?type=meat-and-filler",
+      dataType: 'json'
+    })
+      .done(addP)
+      .fail(function () {
+        alert("FAILURE");
+      })
+  });
 
-function addP (data){
+  function addP(data) {
     $('p').text(data[0])
   }
 
-$("#getBtn").click(function(){
-  $.get('https://api.github.com/users/colt')
-});
+  $("#getBtn").click(function () {
+    $.get('https://api.github.com/users/JamesRileydev')
+      .done(function (data) {
+        console.log(data);
+      })
+      .fail(function () {
+        console.log("Error");
+      })
+  });
 
-$("#postBtn").click(function(){
+  $("#postBtn").click(function () {
+    var data_ = {name: 'Billy', city: "G-rap"};
+    $.post("https://www.goodtimesforall.com", data_)
+      .done(function (data) {
+        console.log(data);
+      })
+      .fail(function () {
+        console.log("Error");
+      })
+  });
 
-});
-
-$("#getJSONBtn").click(function(){
-
-});
+  $("#getJSONBtn").click(function () {
+    $.getJSON('https://api.github.com/users/JamesRileydev')
+    .done(function(data){
+      console.log(data);
+    })
+    .fail(function(){
+      console.log("getJSON failed");
+    })
+  });
 
 
 }
